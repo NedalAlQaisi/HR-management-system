e3f1149e14dbfa216c801eaae27069d29e4ca953
NedalAlQaisi <n.alqaisi221999@gmail.com>
2022-01-27 03:51:08 +0200

    Updated events branch, and fixed some bugs.

--- js/app.js
+++ js/app.js
@@ -1,68 +1,151 @@
-'use strict';
+let arr = [];
 
-let allEmployees = []
 
-function Employee(employeeID, fullName, department, level) {
+let form = document.getElementById("form");
+
+
+function Employee(employeeID, fullName, department, level, img) {
 
     this.employeeID = employeeID;
     this.fullName = fullName;
     this.department = department;
     this.level = level;
     this.salary = 0;
-    this.salaryAfterTax = 0;
-    this.image = image;
-    allEmployees.push(this);
+    this.img = img;
+    arr.push(this);
 }
 
-
-
-
-var department = ["Administration", "Marketing", "Development", "Finance"];
-var level = ["Junior", "Mid-Senior", "Senior"];
-var salaryAfterTax = 0;
-
-//Salary Distribution
-
-Employee.prototype.getSalary = function() {
-    if (this.level == 'Senior')
-        return this.salary = Math.floor(Math.random() * (2000 - 1500 + 1)) + 1500;
-
-    if (this.level == 'Mid-Senior')
-        return this.salary = Math.floor(Math.random() * (1500 - 1000 + 1)) + 1000;
-
-    if (this.level == 'Junior')
-        return this.salary = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
-
-}
-
-//Salary After Tax 
-
-Employee.prototype.getTax = function(salary) {
-    return this.salaryAfterTax = salary - (salary * 0.075);
-}
-
-//Render the outpot
-
-Employee.prototype.render = function() {
-
-    document.write("<h1>EMPLOYEES</h1> <br>");
-
-    for (let i = 0; i < allEmployees.length; i++) {
-        document.write(`<p> <b>Empolyee Name:</b>   ${allEmployees[i].fullName}</p>`);
-        document.write(`<p><b>The Salary:</b>       ${allEmployees[i].getSalary()}</p>`);
-        document.write(`<p><b>Salary After Tax:</b> ${allEmployees[i].getTax(allEmployees[i].salary)}</p>`);
-        document.write('<br> <hr> <br>');
-    }
-
-}
 
 //The data to be stored
 
-let ghazi = new Employee(1000, "GhaziSamer", "Administration", "Senior");
-let lana = new Employee(1001, "LanaAli", "Finance", "Senior");
-let tamara = new Employee(1002, "TamaraAyoub", "Marketing", "Senior");
-let safi = new Employee(1003, "SafiWalid", "Administration", "Mid-Senior");
-let omar = new Employee(1004, "OmarZaid", "Development", "Senior");
-let rana = new Employee(1005, "RanaSaleh", "Development", "Junior");
-let hadi = new Employee(1006, "HadiAhmad", "Finance", "Mid-Senior");
-ghazi.render();
+let Nedal = new Employee(1000, "Nedal", "Administration", "Senior", 'images/Nedal.jpg');
+let Ghazi = new Employee(1001, "Ghazi Samer", "Administration", "Senior", 'images/Ghazi.jpg');
+let Lana = new Employee(1002, "Lana Ali", "Finance", "Senior", 'images/Lana.jpg');
+let Tamara = new Employee(1003, "Tamara Ayoub", "Marketing", "Senior", 'images/Tamara.jpg');
+let Safi = new Employee(1004, "Safi Walid", "Administration", "Mid-Senior", 'images/Safi.jpg');
+let Omar = new Employee(1005, "Omar Zaid", "Development", "Senior", 'images/Omar.jpg');
+let Rana = new Employee(1006, "Rana Saleh", "Development", "Junior", 'images/Rana.jpg');
+let Hadi = new Employee(1007, "Hadi Ahmad", "Finance", "Mid-Senior", 'images/Hadi.jpg');
+
+
+form.addEventListener("submit", addEmbloyee)
+
+function addEmbloyee(event) {
+    event.preventDefault();
+    let fullName = event.target.fullName.value;
+    let department = event.target.department.value;
+    let level = event.target.level.value;
+    let image = event.target.image.value;
+    let newId = generatID();
+    let newEmployee = new Employee(newId, fullName, department, level, image);
+    newEmployee.raNsalary();
+    saveForm();
+    newEmployee.render();
+
+}
+
+
+Employee.prototype.raNsalary = function() {
+    let fSalary = 0;
+    switch (this.level) {
+        case "Junior":
+            fSalary = Math.floor(Math.random() * (1000 - 500)) + 500;
+            break;
+        case "Mid-Senior":
+            fSalary = Math.floor(Math.random() * (1500 - 1000)) + 1000;
+
+            break;
+        case "Senior":
+            fSalary = Math.floor(Math.random() * (2000 - 1500)) + 1500;
+            break;
+        default:
+            console.log("No level")
+            break;
+    }
+
+    //Salary After Tax 
+
+    fSalary = fSalary - ((7.5 * 100) / 100);
+    this.salary = fSalary;
+    return fSalary;
+}
+
+
+function generatID() {
+    var valu = Math.floor(1000 + Math.random() * 9999);
+    return valu;
+}
+
+Employee.prototype.render = function() {
+    var newDiv = document.createElement("div");
+    newDiv.style.backgroundColor = "#219F94";
+    newDiv.style.width = "250px";
+    newDiv.style.padding = "20px";
+    newDiv.style.margin = "20px";
+    newDiv.style.fontSize = "17px";
+    newDiv.style.color = "white";
+
+
+    let image = document.createElement('img');
+    image.setAttribute("src", this.img);
+    newDiv.appendChild(image).width = "210";
+    newDiv.appendChild(image).height = "210";
+    divEmployee.appendChild(newDiv);
+
+    let info = document.createElement('p')
+    info.textContent = "Name : " + this.fullName + " - " + "ID: " + this.employeeID +
+        " " + "Department: " + this.department + "     " + "level : " + this.level + " - " + "Salary: " + this.raNsalary();
+
+    newDiv.appendChild(info);
+
+    divEmployee.appendChild(newDiv);
+}
+
+function renderAll() {
+    arr.forEach(element => {
+        element.render();
+    });
+}
+
+
+function saveForm() {
+    let stringfiedEmployees = JSON.stringify(arr)
+    localStorage.setItem("Employees", stringfiedEmployees);
+
+}
+
+function getForm() {
+
+    let data = localStorage.getItem("Employees");
+    let parsedData = JSON.parse(data);
+    if (parsedData != null) {
+        arr = [];
+        console.log(parsedData);
+        for (let i = 0; i < parsedData.length; i++) {
+            console.log(parsedData[i]);
+            new Employee(parsedData[i].employeeID, parsedData[i].fullName, parsedData[i].department, parsedData[i].level, parsedData[i].img);
+
+        }
+    }
+
+}
+getForm();
+renderAll();
+
+
+
+
+
+
+
+
+// document.write("<h1>EMPLOYEES</h1> <br>");
+
+// for (let i = 0; i < arr.length; i++) {
+// document.write(`<p> <b>Empolyee Name:</b>   ${arr[i].fullName}</p>`);
+// document.write(`<p><b>The Salary:</b>       ${arr[i].getSalary()}</p>`);
+// document.write(`<p><b>Salary After Tax:</b> ${arr[i].getTax(arr[i].salary)}</p>`);
+// document.write('<br> <hr> <br>');
+// }
+
+// }
